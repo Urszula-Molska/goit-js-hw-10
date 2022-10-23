08c40ab7a95529842dc51e58514173c7f48ef887
Urszula Molska <urszula_molska@wp.pl>
2022-10-23 18:25:40 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const countryInfo = document.querySelector('.country-info');
 
 function fetchCountries(countryName) {
   //countryName.toString();
@@ -8,13 +9,12 @@ function fetchCountries(countryName) {
   console.log(typeof countryName);
   return fetch(
     `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response
-      .json()
-      .then(countries => {
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json().then(countries => {
         if (countries.length > 10) {
           console.log(
             'Too many matches found. Please enter a more specific name.'
@@ -29,17 +29,26 @@ function fetchCountries(countryName) {
           }
         }
         if ((countries.length >= 2) & (countries.length <= 10)) {
-          console.log(countries.length);
-          console.log(countries);
-          for (country of countries) {
-            console.log(country.flags.svg);
-            console.log(country.name.official);
-          }
+          let markup = '';
+          countries.forEach(country => {
+            markup += `<div class="country-container">
+              <img class="flag" src="${country.flags.svg}" alt= "flag of ${country.name.official}"/>
+            <h2 class="country">${country.name.official}</h2>
+            </div>`;
+          });
+
+          console.log(markup);
+          countryInfo.insertAdjacentHTML('beforeend', markup);
         }
-      })
-      .catch(error => console.log(error));
-  });
+        //console.log(countries.length);
+        // console.log(countries);
+        //for (country of countries) {
+        //console.log(country.flags.svg);
+        // console.log(country.name.official);
+        //}
+      });
+    })
+    .catch(error => console.log(error));
 }
 
 fetchCountries('con');
-//console.log(countries))
